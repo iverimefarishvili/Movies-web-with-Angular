8274601f14dc1054f1d62d436125e971bb561f45
iverimefarishvili <mefarishvili922@gmail.com>
2019-09-06 17:31:48 +0400

    add

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
   { 
     path: 'auth', 
     loadChildren: './auth/auth.module#AuthModule'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
 
